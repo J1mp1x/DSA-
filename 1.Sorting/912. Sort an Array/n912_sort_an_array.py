89fb37c87af82b47355775899c951e2308d9227f
spake <spakeduard669@gmail.com>
2025-04-12 19:11:00 +0300

    DSA

--- 1.Sorting/912. Sort an Array/n912_sort_an_array.py
+++ 1.Sorting/912. Sort an Array/n912_sort_an_array.py
@@ -9,28 +9,23 @@ class Solution(object):
             left = 2 * i + 1
             right = 2 * i + 2
 
-            # If left child exists and is greater than root
             if left < n and arr[left] > arr[largest]:
                 largest = left
 
-            # If right child exists and is greater than largest so far
             if right < n and arr[right] > arr[largest]:
                 largest = right
 
-            # If largest is not root
             if largest != i:
-                arr[i], arr[largest] = arr[largest], arr[i]  # swap
+                arr[i], arr[largest] = arr[largest], arr[i]
                 heapify(arr, n, largest)
 
         n = len(nums)
 
-        # Build a max heap
         for i in range(n // 2 - 1, -1, -1):
             heapify(nums, n, i)
 
-        # One by one extract elements
         for i in range(n - 1, 0, -1):
-            nums[0], nums[i] = nums[i], nums[0]  # swap
+            nums[0], nums[i] = nums[i], nums[0]
             heapify(nums, i, 0)
 
         return nums
